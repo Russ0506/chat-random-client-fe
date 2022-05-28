6c5b2b3b03dfa632b478d2fca87170ad0f022523
vnskingmotor <57930370+VnsKing001@users.noreply.github.com>
2022-05-28 21:50:09 +0700

    git commit -m HieuTG
     - code UI  drawer side/app bar. Status : implement UI

---
+++ src/views/common/drawer/DrawerAppBar.js
@@ -0,0 +1,17 @@
+import { AppBar } from '@mui/material';
+import React from 'react'
+import {DRAWER_WITH} from '../../../constant/css_constant';
+import AppBarComponent from './component/AppBarComponent';
+export default function DrawerAppBar() {
+  return (
+    <AppBar
+      position="fixed"
+      sx={{
+        width: { sm: `calc(100% - ${DRAWER_WITH}px)` },
+        ml: { sm: `${DRAWER_WITH}px` },
+      }}
+    >
+      <AppBarComponent />
+    </AppBar>
+  );
+}
--- src/views/pages/welcome/Welcome.js
+++ src/views/pages/welcome/Welcome.js
@@ -1,7 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import Header from "../../common/header/Header";
+import Footer from "../../common/footer/Footer";
+import DrawerLayout from '../../common/drawer/DrawerLayout';
 export default function Welcome() {
   return (
-    <div>features add later</div>
-  )
+    <>
+    <DrawerLayout />
+      {/* <Header /> */}
+      {/* <div>features add later</div> */}
+      {/* <Footer /> */}
+    </>
+  );
 }
