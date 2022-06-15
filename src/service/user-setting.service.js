55108bbc55ad8ad5e90fc9db43370ee8ea03be17
Truc Ly Nguyen <truclyy38@gmail.com>
2022-06-16 01:10:54 +0700

    aomplete api for enque and fix some bug switch filter

--- src/service/user-setting.service.js
+++ src/service/user-setting.service.js
@@ -1,17 +1,39 @@
 import { axiosClient } from '../setup/axiosClient'
+import { setMessage } from "../features/message";
 
 const URL = "user_setting"
 
-const saveDataSearch = async (params) => {
+const saveDataSearch = async (params, thunkAPI) => {
   try {
     const res = await axiosClient.post(`${URL}/update_or_create`, params)
     return res
   } catch (error) {
-    console.log(error)
+    const message =
+      (error.response.data.errors[0]) ||
+      error.message ||
+      error.toString();
+    thunkAPI.dispatch(setMessage(message));
+    return thunkAPI.rejectWithValue();
+  }
+};
+
+const getDataSearch = async (thunkAPI) => {
+  try {
+    const res = await axiosClient.get(`${URL}`)
+    console.log(res);
+    return res
+  } catch (error) {
+    const message =
+      (error.response.data.errors[0]) ||
+      error.message ||
+      error.toString();
+    thunkAPI.dispatch(setMessage(message));
+    return thunkAPI.rejectWithValue();
   }
 };
 
 const userSettingService = {
-    saveDataSearch,
+  saveDataSearch,
+  getDataSearch,
 };
 export default userSettingService;
