cb0760f08db66d2e6c8b54b93d4661470581704f
Truc Ly Nguyen <truclyy38@gmail.com>
2022-06-19 18:28:19 +0700

    identity check

--- src/features/auth.js
+++ src/features/auth.js
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { setMessage } from "./message";
 import AuthService from "../service/auth.service";
 const user = JSON.parse(localStorage.getItem("user"));
+
 export const resetPwd = createAsyncThunk(
   "auth/resetPwd",
   async (params, thunkAPI) => {
@@ -75,6 +76,26 @@ export const sendMailResetPass = createAsyncThunk(
 export const logout = createAsyncThunk("auth/logout", async () => {
   await AuthService.logout();
 });
+
+export const user_verify = createAsyncThunk(
+  "auth/user_verify",
+  async (params, thunkAPI) => {
+    try {
+      let response = await AuthService.userVerify();
+      return response;
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue();
+    }
+  }
+);
+
 const initialState = user
   ? { isLoggedIn: true, user }
   : { isLoggedIn: false, user: null };
