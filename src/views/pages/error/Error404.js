81b5d9d13801a9c559af467d7c7f78fc212068df
bopexe <hoangnguyen1999ht@gmail.com>
2022-06-21 01:44:35 +0700

    fixbug

--- src/views/pages/error/Error404.js
+++ src/views/pages/error/Error404.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { m } from "framer-motion";
-import { Button } from "@mui/material";
-import React, { Component } from "react";
-import "../../../styles/Error.css";
-=======
-// import {m} from 'framer-motion';
-
-
->>>>>>> aca9a98f82d0edd0523c4b7efa7fae2f4e347071
 import image from "../../../assets/img/illustration_404.svg";
 import "../../../styles/Error.css";
 
@@ -16,23 +6,28 @@ import "../../../styles/Error.css";
 // Có thể bởi vì những file này đã bị xóa, hoặc chưa từng tồn tại trước đây.
 // Nguyên nhân thường là do lỗi chính tả trong URL.
 
-export default function Error404 () {
-    return (
-      <div id="error404">
-        <div id="error-text">
-          <span>Sorry, page not found!</span>
-          <p>
-            Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve
-            mistyped the URL? Be sure to check your spelling.
-          </p>
-          <img src={image} alt="image"></img>
-          <div id="copyright">
-            <br></br>
-            <a className="back" title="Igniel" href="/welcome" style={{color: "white"}}>
-              Go home
-            </a>
-          </div>
+export default function Error404() {
+  return (
+    <div id="error404">
+      <div id="error-text">
+        <span>Sorry, page not found!</span>
+        <p>
+          Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve
+          mistyped the URL? Be sure to check your spelling.
+        </p>
+        <img src={image} alt="image"></img>
+        <div id="copyright">
+          <br></br>
+          <a
+            className="back"
+            title="Igniel"
+            href="/welcome"
+            style={{ color: "white" }}
+          >
+            Go home
+          </a>
         </div>
       </div>
-    )
+    </div>
+  );
 }
