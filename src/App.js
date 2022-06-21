3651624cc481b090e5fbef61d8cb3593ba009a2b
vnskingmotor <57930370+VnsKing001@users.noreply.github.com>
2022-06-21 17:06:11 +0700

    git commit -m
     - config router whenever not login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "./styles/App.css"
 import "./styles/loading.css"
 import { BrowserRouter} from "react-router-dom";
 import Routes from "./routes";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { GRP_COLOR} from "./constant/css_constant"
+import { user_verify } from "./features/auth";
+import { useDispatch } from "react-redux";
+import SignIn from "./views/pages/auth/Login";
 
 function App() {
 
@@ -23,6 +26,7 @@ function App() {
       // mode: GRP_COLOR.CODE016,
     },
   });
+
   
   return (
     <ThemeProvider theme={themeDefault}>
