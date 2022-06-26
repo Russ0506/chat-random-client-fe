ab9ab86e8ceb7cfe1836c10d7520efb9112aa69d
Nguyen Huy Hoang <hoangnguyen1999ht@email.com>
2022-06-26 10:59:14 +0700

    fix welcome page

--- src/views/welcome/Welcome.js
+++ src/views/welcome/Welcome.js
@@ -8,7 +8,7 @@ export default function Welcome() {
       {/* <!-- Navbar Section --> */}
       <nav className="navbar">
         <a href="/" className="navbar__logo">
-          Cherish by chating
+          Cherish by chatting
         </a>
         <div className="navbar__bars">
           <i className="fas fa-bars"></i>
@@ -18,10 +18,18 @@ export default function Welcome() {
             Home
           </a>
           <a href="/" className="navbar__menu--links">
-            Products
+            Features
           </a>
           <a href="/" className="navbar__menu--links">
-            Services
+            FAQ
+          </a>
+          <a
+            href="/register"
+            className="navbar__menu--links"
+            id="button_Signup"
+            color="#7347c1"
+          >
+            Sign Up
           </a>
           <a href="/users/login" className="navbar__menu--links" id="button">
             Sign In
@@ -33,18 +41,28 @@ export default function Welcome() {
       <div className="hero">
         <div className="hero__container">
           <div className="hero__container--left">
-            <h1>Start a new friend</h1>
-            <h2>With Cherish</h2>
-            <p>Sign up now to chat.</p>
+            <h1>Stranger Meetup in</h1>
+            <h2>Cherish chatting</h2>
+            <p>Meet People Like Never Before</p>
             <button className="hero__container--btn">
-              <a href="/register">Sign Up</a>
+              <a href="/register">Get Started</a>
             </button>
           </div>
           <div className="hero__container--right">
-            <input type="image" src={image} alt="image" className="hero__container--img" />
+            <input
+              type="image"
+              src={image}
+              alt="image"
+              className="hero__container--img"
+            />
           </div>
         </div>
       </div>
+      <footer>
+        <div className="col">
+          <small>© 2022 cherishbychatting.com All Rights Reserved</small>
+        </div>
+      </footer>
     </>
   );
 }
