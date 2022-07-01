72116bfb1403ae9bcb3c3f91edc4452d710be83e
vnskingmotor <57930370+VnsKing001@users.noreply.github.com>
2022-07-01 16:55:51 +0700

    code UI chat maintain (remove nav for update later)

--- src/views/chat/areaChat/sendMessageBox/ChatMessageInput.js
+++ src/views/chat/areaChat/sendMessageBox/ChatMessageInput.js
@@ -1,23 +1,28 @@
-import PropTypes from 'prop-types';
-import { useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from "prop-types";
+import { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 // @mui
-import { styled } from '@mui/material/styles';
-import { Stack, Input, Divider, IconButton, InputAdornment } from '@mui/material';
-import Iconify from '../../../common/base/icon/Iconify';
-import EmojiPicker from '../../../common/base/emoji/EmojiPicker'
-import { sendMesage } from '../../../../features/chat';
+import { styled } from "@mui/material/styles";
+import {
+  Stack,
+  Input,
+  Divider,
+  IconButton,
+  InputAdornment,
+} from "@mui/material";
+import Iconify from "../../../common/base/icon/Iconify";
+import EmojiPicker from "../../../common/base/emoji/EmojiPicker";
+import { sendMesage } from "../../../../features/chat";
 // utils
 // components
 
-
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({ theme }) => ({
+const RootStyle = styled("div")(({ theme }) => ({
   minHeight: 56,
-  display: 'flex',
-  position: 'relative',
-  alignItems: 'center',
+  display: "flex",
+  position: "relative",
+  alignItems: "center",
   paddingLeft: theme.spacing(2),
 }));
 
@@ -29,38 +34,42 @@ ChatMessageInput.propTypes = {
   onSend: PropTypes.func,
 };
 
-export default function ChatMessageInput({ disabled, conversation, onSend, recipientId }) {
-  const dispatch = useDispatch()
+export default function ChatMessageInput({
+  disabled,
+  conversation,
+  onSend,
+  recipientId,
+}) {
+  const dispatch = useDispatch();
   const fileInputRef = useRef(null);
 
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   const handleAttach = () => {
     fileInputRef.current?.click();
   };
 
   const handleKeyUp = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSend();
     }
   };
 
   const handleSend = () => {
     if (!message) {
-      return '';
+      return "";
     }
     if (onSend && conversation.id) {
       let params = {
-        conversationId :conversation.id,
+        conversationId: conversation.id,
         text: message,
         recipient_id: recipientId,
-      }
+      };
       onSend(params);
 
-      dispatch(sendMesage(params))
-
+      dispatch(sendMesage(params));
     }
-    return setMessage('');
+    return setMessage("");
   };
 
   return (
@@ -73,6 +82,14 @@ export default function ChatMessageInput({ disabled, conversation, onSend, recip
         onKeyUp={handleKeyUp}
         onChange={(event) => setMessage(event.target.value)}
         placeholder="Type a message"
+        sx={{
+          borderRadius: "10px",
+          border: "0.2px solid #f6f6f6",
+          paddingLeft: "10px",
+          paddingTop: "5px",
+          paddingBottom: "5px",
+          background: "#f6f6f6",
+        }}
         // startAdornment={
         //   <InputAdornment position="start">
         //     <EmojiPicker disabled={disabled} value={message} setValue={setMessage} />
@@ -81,9 +98,17 @@ export default function ChatMessageInput({ disabled, conversation, onSend, recip
         endAdornment={
           <Stack direction="row" spacing={1} sx={{ flexShrink: 0, mr: 1.5 }}>
             <IconButton disabled={disabled} size="small" onClick={handleAttach}>
-              <Iconify icon="ic:round-add-photo-alternate" width={22} height={22} />
+              <Iconify
+                icon="ic:round-add-photo-alternate"
+                width={22}
+                height={22}
+              />
             </IconButton>
-            <EmojiPicker disabled={disabled} value={message} setValue={setMessage} />
+            <EmojiPicker
+              disabled={disabled}
+              value={message}
+              setValue={setMessage}
+            />
             <IconButton disabled={disabled} size="small">
               <Iconify icon="eva:mic-fill" width={22} height={22} />
             </IconButton>
@@ -91,13 +116,18 @@ export default function ChatMessageInput({ disabled, conversation, onSend, recip
         }
       />
 
-      <Divider orientation="vertical" flexItem />
+      {/* <Divider orientation="vertical" flexItem /> */}
 
-      <IconButton color="primary" disabled={!message} onClick={handleSend} sx={{ mx: 1 }}>
-        <Iconify icon="ic:round-send" width={22} height={22} />
+      <IconButton
+        color="primary"
+        disabled={!message}
+        onClick={handleSend}
+        sx={{ mx: 1 }}
+      >
+        <Iconify icon="ic:round-send" width={25} height={25} />
       </IconButton>
 
-      <input type="file" ref={fileInputRef} style={{ display: 'none' }} />
+      <input type="file" ref={fileInputRef} style={{ display: "none" }} />
     </RootStyle>
   );
 }
