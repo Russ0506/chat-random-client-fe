9e52c77b733a65aa45534eb82231e25700bda74a
Truc Ly Nguyen <truclyy38@gmail.com>
2022-08-05 01:54:06 +0700

    integrate api for admin module

--- src/views/admin/mock_data.js
+++ src/views/admin/mock_data.js
@@ -182,3 +182,53 @@ export const userList = [
     reportCount: 234234,
   },
 ];
+
+export const userList2 = [
+  {
+    "id": 54,
+    "name": "test 19",
+    "email":"lyxinhdep@gmail.com",
+    "avatar_path": "/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBEQT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--005f872734dbe54bf351f256aca7cb74d43a1cab/Screen%20Shot%202022-06-27%20at%2015.52.56.png",
+    "last_online": null,
+    "no_of_reports": 7,
+    "no_of_unresolved_reports": 6,
+    "is_online": false
+  },
+  {
+    "id": 53,
+    "name": "test 20",
+    "avatar_path": "/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBEQT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--005f872734dbe54bf351f256aca7cb74d43a1cab/Screen%20Shot%202022-06-27%20at%2015.52.56.png",
+    "last_online": null,
+    "no_of_reports": 7,
+    "no_of_unresolved_reports": 10,
+    "is_online": false
+  }
+]
+
+export const mockData3 = [{
+  "id": 9,
+  "name": "reporter 1",
+  "email":"reporter_1@gmail.com",
+  "text": "khong duoc van minh",
+  "status": "unresolved",
+  "created_at": "2022-12-01",
+  "problem_type": "violence",
+  "attachment_paths": [
+      "/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBFZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--bb0121589473c9a47c36cba0d47ce71e470a721c/Screen%20Shot%202022-07-25%20at%2010.00.52.png"
+  ],
+  "owner_id": 35
+},
+{
+  "id": 10,
+  "name": "reporter 2",
+  "email":"reporter_2@gmail.com",
+  "text": "qua lao",
+  "status": "unresolved",
+  "created_at": "2022-12-01",
+  "problem_type": "violence",
+  "attachment_paths": [
+      "/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBFdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--ef07f2bafb92cdb2480063beb465f67a24fa79da/Screen%20Shot%202022-07-25%20at%2010.00.52.png"
+  ],
+  "owner_id": 35
+}
+]
